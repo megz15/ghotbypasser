29bc449f89e67ab87c8037c83f34bc9b906cee68
megz15 <meghraj.g16@gmail.com>
2023-01-16 13:16:24 +0530

    Pretty-fied site

--- src/lib/LinkInput.svelte
+++ src/lib/LinkInput.svelte
@@ -3,20 +3,29 @@ export let link
 import Output from './Output.svelte'
 let out
 let parse = () => {
-    // fetch(`https://proxy.cors.sh/${link}`)
-    // .then(d => d.text())
-    // .then(d => {
-    //     let b = d.search('poster="')+8
-    //     out = d.slice(b, d.indexOf('_l', b)).replace('previews', 'encoded') + '.webm'
-    // })
-    out = 'https://cdn.numerade.com/encoded/758b4d3a-b577-4ab7-ac11-320d7b4779d6.webm'
+    out = 'load'
+    fetch(`https://proxy.cors.sh/${link}`)
+    .then(d => d.text())
+    .then(d => {
+        let b = d.search('poster="')+8
+        out = d.slice(b, d.indexOf('_l', b)).replace('previews', 'encoded') + '.webm'
+    })
+    // out = 'https://cdn.numerade.com/encoded/758b4d3a-b577-4ab7-ac11-320d7b4779d6.webm'
 }
 if (link!=null){parse()}
 </script>
 
-<input placeholder="Paste link here" value="{link}" on:keyup={e=>e.key==='Enter'&&parse()}>
+<input placeholder="Paste link here" bind:value="{link}" on:keyup={e=>e.key==='Enter'&&parse()}>
 
-{#if out!=null}
+{#if out==null}
+<div>
+    null
+</div>
+{:else if out=='load'}
+<div>
+    loading
+</div>
+{:else}
 <div class="card">
     <Output name="{out}"/>
 </div>
--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -3,3 +3,7 @@
 </script>
 
 <p><a href="{name}">{name}</a></p>
+<video controls width='100%' style='border-radius:8px;'>
+    <track kind="captions">
+    <source src={name} type='video/webm'>
+</video>
