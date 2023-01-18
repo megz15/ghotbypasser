0d8ff1a95e91084f6ed1c741129615924d9503f5
megz15 <meghraj.g16@gmail.com>
2023-01-18 11:19:38 +0530

    Fixed link error handling

--- src/lib/LinkInput.svelte
+++ src/lib/LinkInput.svelte
@@ -15,7 +15,8 @@ let parse = () => {
     .then(d => d.text())
     .then(d => {
         let b = d.search('poster="')+8
-        out = (d.slice(b, d.indexOf('_l', b)) + '.webm').replace('previews', (link.includes('/ask/question/')) ? 'ask_video' : 'encoded')
+        // out = (d.slice(b, d.indexOf('_l', b)) + '.webm').replace('previews', (link.includes('/ask/question/')) ? 'ask_video' : 'encoded')
+        out = (d.slice(b, d.indexOf('_l', b)) + '.webm').replace('previews', 'encoded')
         // console.log(d)
     })
     // out = 'https://cdn.numerade.com/encoded/758b4d3a-b577-4ab7-ac11-320d7b4779d6.webm'
--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -1,11 +1,18 @@
 <script>
     export let name
+    let vid
 </script>
 
 <p><a href="{name}">{name}</a></p>
-<video controls width='100%' style='border-radius:8px;'>
+<video controls width='100%' style='border-radius:8px;' bind:this={vid}>
     <track kind="captions">
-    <source src={name} type='video/webm'>
+    <source src={name} type='video/webm' on:error={
+        e => {
+            name = name.replace('encoded', 'ask_video')
+            e.target['src'] = name
+            vid.load()
+        }
+    }>
     <!-- on:error={
         e => {
             name = name.replace('encoded', 'ask_video')
