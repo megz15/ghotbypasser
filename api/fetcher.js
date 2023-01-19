9799cc4e9ddf6f9e2d60e2f9e3302645bd6856b1
megz15 <meghraj.g16@gmail.com>
2023-01-19 18:50:22 +0530

    srvls tst 3

--- api/fetcher.js
+++ api/fetcher.js
@@ -1,5 +1,5 @@
 export default function hello(req, res) {
     res.statusCode = 200;
     const link = req.query.link;
-    fetch(link).then(d => res.json({ link: d.text(), message: 'OK' }))
+    fetch(link).then(d => d.text()).then(d => res.json({ link: d, message: 'OK' }))
 }
