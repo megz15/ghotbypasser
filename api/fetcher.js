604e525e2f0e49ecd0e34d11738c34407a00c0fe
megz15 <meghraj.g16@gmail.com>
2023-01-19 18:43:58 +0530

    serverless fn link test

--- api/fetcher.js
+++ api/fetcher.js
@@ -1,4 +1,16 @@
-export default function fetcher(req, res) {
+export default function hello(req, res) {
     res.statusCode = 200;
-    res.json({ message: 'Passed' });
+    const link = req.query;
+    let out;
+    fetch(`https://proxy.cors.sh/${link}`,{
+        headers: {
+            'x-cors-api-key': import.meta.env.VITE_API_KEY,
+        }
+    })
+    .then(d => d.text())
+    .then(d => {
+        let b = d.search('poster="')+8
+        out = (d.slice(b, d.indexOf('_l', b)) + '.webm').replace('previews', 'encoded')
+    })
+    res.json({ link: out, message: 'OK' });
 }
