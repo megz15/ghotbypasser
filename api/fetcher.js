48fdcb26da7d5377a54d97083b9b9412d3cc4327
megz15 <meghraj.g16@gmail.com>
2023-01-19 18:49:12 +0530

    srvls tst 2

--- api/fetcher.js
+++ api/fetcher.js
@@ -1,16 +1,5 @@
 export default function hello(req, res) {
     res.statusCode = 200;
-    const link = req.query;
-    let out;
-    fetch(`https://proxy.cors.sh/${link}`,{
-        headers: {
-            'x-cors-api-key': import.meta.env.VITE_API_KEY,
-        }
-    })
-    .then(d => d.text())
-    .then(d => {
-        let b = d.search('poster="')+8
-        out = (d.slice(b, d.indexOf('_l', b)) + '.webm').replace('previews', 'encoded')
-    })
-    res.json({ link: out, message: 'OK' });
+    const link = req.query.link;
+    fetch(link).then(d => res.json({ link: d.text(), message: 'OK' }))
 }
