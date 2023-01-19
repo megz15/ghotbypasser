52093bc0adcbbb0cb7af7f1eaf7bc6a87e8255a3
megz15 <meghraj.g16@gmail.com>
2023-01-19 12:59:56 +0530

    Added links to github

--- src/lib/LinkInput.svelte
+++ src/lib/LinkInput.svelte
@@ -26,8 +26,8 @@ if (link!=null){parse()}
 
 <input placeholder="Paste link here" bind:value="{link}" on:keyup={e=>e.key==='Enter'&&parse()}>
 <div class="btn-grp">
-    <button>Star me on GitHub!</button>
-    <button>Found a bug?</button>
+    <button on:click={_ => window.open("https://github.com/megz15/ghotbypasser", '_blank').focus()}>Star me on GitHub!</button>
+    <button on:click={_ => window.open("https://github.com/megz15/ghotbypasser/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc", '_blank').focus()}>Found a bug?</button>
 </div>
 
 {#if out==null}
