3da72f16e5199d4b1eb9a2d92756fef21f3018eb
megz15 <meghraj.g16@gmail.com>
2023-04-04 20:31:04 +0530

    edited supported sites section

--- src/lib/Supported.svelte
+++ src/lib/Supported.svelte
@@ -1,3 +1,9 @@
 <div class="card">
-    <div style="margin: 10px">Note: Only Numerade links are supported for now</div>
+    <div style="margin: 10px">
+        <h2>Supported Sites</h2>
+        <div class="btn-grp">
+            <button on:click={_ => window.open("https://www.numerade.com/")}>Numerade</button>
+            <button on:click={_ => window.open("https://www.quizlet.com/")}>Quizlet</button>
+        </div>
+    </div>
 </div>
