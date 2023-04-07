68c06e7718c7575e67df44f48d8480f3851bb3c8
megz15 <meghraj.g16@gmail.com>
2023-04-07 15:39:54 +0530

    Supported section css fix

--- src/lib/Supported.svelte
+++ src/lib/Supported.svelte
@@ -1,9 +1,7 @@
 <div class="card">
-    <div style="margin: 10px">
-        <h2>Supported Sites</h2>
-        <div class="btn-grp">
-            <button on:click={_ => window.open("https://www.numerade.com/")}>Numerade</button>
-            <button on:click={_ => window.open("https://www.quizlet.com/")}>Quizlet</button>
-        </div>
+    <h2>Supported Sites</h2>
+    <div class="btn-grp">
+        <button style="margin-left:10px;" on:click={_ => window.open("https://www.numerade.com/")}>Numerade</button>
+        <button style="margin-right:10px;" on:click={_ => window.open("https://www.quizlet.com/")}>Quizlet</button>
     </div>
 </div>
